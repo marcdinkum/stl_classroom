77954cdfab2d1761cdc8a6355b41922a230180f5
Marc Groenewegen <marcg@dinkum.nl>
2021-05-18 17:47:33 +0200

    working with iterator

--- artist_vector.cpp
+++ artist_vector.cpp
@@ -10,9 +10,11 @@
 int main()
 {
 std::vector<std::string> artists;
+std::vector<std::string>::iterator index;
 
   std::cout << "Vector contains " << artists.size()
     << " elements" << std::endl;
+  std::cout << "Vector capacity " << artists.capacity() << std::endl;
 
   // Loop until user types quit
   std::cout << "\n\nPlease enter artist names or quit to end and show a list of artists\n";
@@ -25,6 +27,24 @@ std::vector<std::string> artists;
     artists.push_back(nextItem);
     std::cout << "Vector capacity " << artists.capacity() << std::endl;
   } while(true);
+
+
+  std::cout << "\n\nSummary of vector properties:\n";
+  std::cout << "Vector contains " << artists.size() << " elements" << std::endl;
+  std::cout << "Vector capacity " << artists.capacity() << std::endl;
+
+
+  /* vector::end() returns an iterator referring to the past-the-end element
+     which is the theoretical element that would follow the last element
+     in the vector.
+     It does not point to any element, and thus shall not be dereferenced.
+  */
+  std::cout << "\n\nContents of artist database:\n";
+  index=artists.begin();
+  while(index != artists.end()){
+    std::cout << *index << std::endl;
+    index++;
+  } // while
 
 
 
