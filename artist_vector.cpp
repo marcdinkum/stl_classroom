439d91d5ba28df2635d4455aa630ab30675b7aae
Marc Groenewegen <marcg@dinkum.nl>
2021-05-18 17:40:10 +0200

    enter artist names

--- artist_vector.cpp
+++ artist_vector.cpp
@@ -11,7 +11,22 @@ int main()
 {
 std::vector<std::string> artists;
 
-  std::cout << "Vector contains " << artists.size() << " elements" << std::endl;
+  std::cout << "Vector contains " << artists.size()
+    << " elements" << std::endl;
+
+  // Loop until user types quit
+  std::cout << "\n\nPlease enter artist names or quit to end and show a list of artists\n";
+  std::string nextItem;
+  do
+  {
+    std::cout << "Artist name: ";
+    std::cin >> nextItem;
+    if(nextItem == "quit") break;
+    artists.push_back(nextItem);
+    std::cout << "Vector capacity " << artists.capacity() << std::endl;
+  } while(true);
+
+
 
   return 0;
 } // main()
