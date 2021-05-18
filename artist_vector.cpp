6715cbc1c1d2982e456be7d37c14249c6d904b44
Marc Groenewegen <marc.dinkum@gmail.com>
2021-05-18 14:49:37 +0200

    Create artist_vector.cpp

---
+++ artist_vector.cpp
@@ -0,0 +1,15 @@
+/*
+ STL Vector example
+
+ A list of artists that can be sorted by name
+*/
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+std::vector<std::string> artists;
+
+  return 0;
+} // main()
